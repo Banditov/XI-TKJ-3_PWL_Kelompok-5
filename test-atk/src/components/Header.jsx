dff7e46308ae07b31bf424900d7d653ca31d21e0
Banditov <vito20samsung21@gmail.com>
2025-09-18 17:39:53 +0700

    new file:   test-atk/.gitignore 	new file:   test-atk/README.md 	new file:   test-atk/eslint.config.js 	new file:   test-atk/index.html 	new file:   test-atk/package-lock.json 	new file:   test-atk/package.json 	new file:   test-atk/public/vite.svg 	new file:   test-atk/src/assets/Back-end/resources/images/bg-12.png 	new file:   test-atk/src/assets/Back-end/resources/images/buku_tulis_besar.png 	new file:   test-atk/src/assets/Back-end/resources/images/buku_tulis_folio.png 	new file:   test-atk/src/assets/Back-end/resources/images/buku_tulis_pink.png 	new file:   test-atk/src/assets/Back-end/resources/images/ct-522.png 	new file:   test-atk/src/assets/Back-end/resources/images/jk-100.png 	new file:   test-atk/src/assets/Back-end/resources/images/joyko_eraser.png 	new file:   test-atk/src/assets/Back-end/resources/images/k-03_gel_pen.png 	new file:   test-atk/src/assets/Back-end/resources/images/mp-clipper.png 	new file:   test-atk/src/assets/Front-end/resources/font/montserrat-500.woff2 	new file:   test-atk/src/assets/Front-end/resources/font/montserrat-800.woff2 	new file:   test-atk/src/assets/Front-end/resources/font/montserrat-regular.woff2 	new file:   test-atk/src/assets/Front-end/resources/image/account.png 	new file:   test-atk/src/assets/Front-end/resources/image/add.png 	new file:   test-atk/src/assets/Front-end/resources/image/banner.png 	new file:   test-atk/src/assets/Front-end/resources/image/bannerStationery.png 	new file:   test-atk/src/assets/Front-end/resources/image/book.png 	new file:   test-atk/src/assets/Front-end/resources/image/bookBanner.png 	new file:   test-atk/src/assets/Front-end/resources/image/category.png 	new file:   test-atk/src/assets/Front-end/resources/image/email.png 	new file:   test-atk/src/assets/Front-end/resources/image/history.png 	new file:   test-atk/src/assets/Front-end/resources/image/home.png 	new file:   test-atk/src/assets/Front-end/resources/image/login_background.png 	new file:   test-atk/src/assets/Front-end/resources/image/logo.png 	new file:   test-atk/src/assets/Front-end/resources/image/pen.png 	new file:   test-atk/src/assets/Front-end/resources/image/phone.png 	new file:   test-atk/src/assets/Front-end/resources/image/wishlist.png 	new file:   test-atk/src/assets/Front-end/resources/image/wishlistFeature.png 	new file:   test-atk/src/components/Header.css 	new file:   test-atk/src/components/Header.jsx 	new file:   test-atk/src/index.css 	new file:   test-atk/src/main.jsx 	new file:   test-atk/vite.config.js

---
+++ test-atk/src/components/Header.jsx
@@ -0,0 +1,53 @@
+import logo from '../assets/Front-end/resources/image/logo.png'
+import account from '../assets/Front-end/resources/image/account.png'
+import home from '../assets/Front-end/resources/image/home.png'
+import category from '../assets/Front-end/resources/image/category.png'
+import history from '../assets/Front-end/resources/image/history.png'
+import wishlist from '../assets/Front-end/resources/image/wishlist.png'
+import './Header.css'
+
+function Header() {
+
+    return (
+    <>
+        <div id="header">
+            <img src={logo} id="logo" />
+            <a href="/Front-end/Login/login.html">
+                <img src={account} class="accountIcon" />
+            </a>
+        </div>
+        <div id="nav">
+            <div class="navSection">
+                <a href="home.html">
+                    <div className="navTab">
+                        <img src={home} />
+                        <p id="selectedTab">HOME</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div className="navTab">
+                        <img src={category} />
+                        <p>SHOP BY <b>CATEGORY</b></p>
+                    </div>
+                </a>
+            </div>
+            <div className="navSection">
+                <a href="">
+                    <div className="navTab">
+                        <img src={history} />
+                        <p>HISTORY</p>
+                    </div>
+                </a>
+                <a href="">
+                    <div className="navTab">
+                        <img src={wishlist} />
+                        <p>WISHLIST</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </>
+    )
+}
+
+export default Header
