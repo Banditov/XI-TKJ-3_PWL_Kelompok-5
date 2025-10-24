4389cc135a3d125105cbb88c68684854120e9ed6
nikoe-ee <nicholas.tantan2009@gmail.com>
2025-10-24 11:07:00 +0700

    new file:   front-end/pages/faq/faq.php

---
+++ front-end/pages/faq/faq.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <title>ATK SKI - Home Page</title>
+    </head>
+    <body>
+
+<!-- Header -->
+
+        <?php include '../../components/header/header.php'; ?>
+
+        <?php include '../../components/footer/footer.php'; ?> 
+    </body>
+</html>
