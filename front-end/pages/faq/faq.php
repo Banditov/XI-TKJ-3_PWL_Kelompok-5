d6255fe86c2f40b8bbb24988b0fd43757d72fc1f
Banditov <vito20samsung21@gmail.com>
2025-10-30 20:52:45 +0700

    modified:   front-end/pages/faq/faq.php

--- front-end/pages/faq/faq.php
+++ front-end/pages/faq/faq.php
@@ -4,8 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <title>ATK SKI - FAQ</title>
-        <link rel="stylesheet" type="text/css" href="/front-end/pages/faq/styles/responsive.css">
         <link rel="stylesheet" type="text/css" href="/front-end/pages/faq/styles/style.css">
+        <link rel="stylesheet" type="text/css" href="/front-end/pages/faq/styles/responsive.css">
     </head>
     <body>
 
