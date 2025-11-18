c1fc608c7d07ac50d3184b50addeca57535d5bf6
Banditov <vito20samsung21@gmail.com>
2025-11-19 01:50:54 +0700

    new file:   front-end/pages/profile/index.php 	new file:   front-end/pages/profile/styles/responsive.css 	new file:   front-end/pages/profile/styles/style.css

---
+++ front-end/pages/profile/index.php
@@ -0,0 +1,50 @@
+<?php
+    require_once __DIR__ . '/../../../back-end/actions/users/session-check.php';
+?>
+
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<title>ATK SKI - TITLE</title>
+		<link rel="icon" type="image/x-icon" href="/front-end/global/resources/image/logo/logo.ico">
+		<link rel="stylesheet" type="text/css" href="/front-end/pages/profile/styles/style.css">
+		<link rel="stylesheet" type="text/css" href="/front-end/global/styles/globalStyle.css">
+		<link rel="stylesheet" type="text/css" href="/front-end/pages/profile/styles/responsive.css">
+	</head>
+	<body>
+
+<!-- Loading Screen -->
+
+		<?php include '../../components/loading-screen/loading-screen.php'; ?>
+
+<!-- Header -->
+
+		<?php include '../../components/header/header.php'; ?>
+
+		<div id="content">
+
+<!-- Page Title -->
+
+			<div id="pageTitle">
+				<b>TITLE</b>
+			</div>
+
+<!-- Content -->
+
+			<div>
+			
+			</div>
+
+		</div>
+
+<!-- Footer -->
+
+		<?php include '../../components/footer/footer.php'; ?> 
+
+<!-- Scripts -->
+
+		<script src="/front-end/global/scripts/loading-screen.js"></script>
+	</body>
+</html>
